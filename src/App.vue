dfc2e92209495c0861445fc3d1ec9fc04c34ca80
Ryo Murakami <ryo.a.murakami@dena.com>
2019-05-20 17:16:36 +0900

    Up date high score.

--- src/App.vue
+++ src/App.vue
@@ -61,6 +61,10 @@ export default {
       this.buttonDisabled = false;
       this.stopTimer();
       this.stopMoles();
+      this.updateHighScore();
+    },
+    updateHighScore: function() {
+      this.highScore = Math.max(this.highScore, this.score);
     },
     startTimer: function() {
       this.timerId = setInterval(() => {
